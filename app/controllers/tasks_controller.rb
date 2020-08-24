2b00061bf27c1da12fa7f3b4389996e6b74fff08
necocoa <43537209+necocoa@users.noreply.github.com>
2020-08-24 18:16:11 +0900

    タスク編集の機能・画面を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,17 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
   end
+
+  def edit; end
 
   def create
     @task = Task.new(task_params)
@@ -21,7 +23,19 @@ class TasksController < ApplicationController
     end
   end
 
+  def update
+    if @task.update(task_params)
+      redirect_to @task, notice: 'タスクを編集しました。'
+    else
+      render :edit
+    end
+  end
+
   private
+
+    def set_task
+      @task = Task.find(params[:id])
+    end
 
     def task_params
       params.require(:task).permit(:name, :description)
