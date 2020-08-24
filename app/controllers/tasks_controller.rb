68e94a803f86bdc10b2a9f084fd66ab60dcbfbab
necocoa <43537209+necocoa@users.noreply.github.com>
2020-08-24 18:14:51 +0900

    タスク作成機能・画面の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,24 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task, notice: 'タスクを作成しました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:name, :description)
+    end
 end
