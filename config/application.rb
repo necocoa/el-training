1107487beea97e33d27cfb0625fa1edfa05cee9e
necocoa <43537209+necocoa@users.noreply.github.com>
2020-08-26 18:07:31 +0900

    タイムゾーンを東京に変更

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module ElTraining
     # Default locale to japanese
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+    config.time_zone = 'Asia/Tokyo'
 
     config.generators do |g|
       # Template engine setting
