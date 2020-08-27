bfc4978f07f4a3bb784680e9f145fbe9dcf4da5d
necocoa <43537209+necocoa@users.noreply.github.com>
2020-08-27 18:05:57 +0900

    タスクを終了期限順でソート切り替えできるようにした

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,11 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { in: 1..50 }
   validates :description, length: { maximum: 1000 }
+
+  scope :end_date_sort, lambda { |sort_method|
+    case sort_method.downcase
+    when 'asc' then order(end_date: :asc)
+    when 'desc' then order('end_date IS NULL, end_date desc')
+    end
+  }
 end
