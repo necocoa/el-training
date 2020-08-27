649bdbe5801994036612bfd0962b642a29a30a7b
necocoa <43537209+necocoa@users.noreply.github.com>
2020-08-27 18:18:18 +0900

    nilの可能性ある変数にぼっち演算子を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ApplicationRecord
   validates :description, length: { maximum: 1000 }
 
   scope :end_date_sort, lambda { |sort_method|
-    case sort_method.downcase
+    case sort_method&.downcase
     when 'asc' then order(end_date: :asc)
     when 'desc' then order('end_date IS NULL, end_date desc')
     end
