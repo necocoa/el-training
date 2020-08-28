a8a24729abc9571c201c5595a72aa562d57e0a28
necocoa <43537209+necocoa@users.noreply.github.com>
2020-08-28 11:16:54 +0900

    end_dateのソートフラグ命名を変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,8 +2,8 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { in: 1..50 }
   validates :description, length: { maximum: 1000 }
 
-  scope :end_date_sort, ->(sort_method) do
-    case sort_method&.downcase
+  scope :end_date_sort, ->(sort_flag) do
+    case sort_flag&.downcase
     when 'asc' then order(end_date: :asc)
     when 'desc' then order('end_date IS NULL, end_date desc')
     end
