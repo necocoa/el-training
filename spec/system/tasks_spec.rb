8e161e4b50d34d36ae276863adab3bdb4c2e5e5c
necocoa <43537209+necocoa@users.noreply.github.com>
2020-08-31 15:51:04 +0900

    describeを追加した

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,98 +2,102 @@ require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system do
   describe 'index' do
-    it 'タスクが作成日の降順で表示される' do
-      old_task = create(:task, name: '古いタスク')
-      new_task = create(:task, name: '新しいタスク')
-      old_task.update(name: '古いタスクを更新') # 更新しても順番が変わらないことを確認するため
+    describe 'sort' do
+      it 'タスクが作成日の降順で表示される' do
+        old_task = create(:task, name: '古いタスク')
+        new_task = create(:task, name: '新しいタスク')
+        old_task.update(name: '古いタスクを更新') # 更新しても順番が変わらないことを確認するため
 
-      visit tasks_path
-      tasks = all('.task_list')
-      expect(tasks[0]).to have_content new_task.name
-      expect(tasks[1]).to have_content old_task.name
-    end
-
-    it 'タスクを終了期限でソートできる' do
-      slow_end_date_task = create(:task, name: '期限が遅いタスク', end_date: '2020-02-01')
-      early_end_date_task = create(:task, name: '期限が早いタスク', end_date: '2020-01-01')
-
-      visit tasks_path
-
-      # ソートリンクを押すと初回は昇順でソートされる
-      click_link Task.human_attribute_name(:end_date)
-      tasks = all('.task_list')
-      expect(tasks[0]).to have_content early_end_date_task.name
-
-      # ソートリンクを押すと2回目は降順でソートされる
-      click_link Task.human_attribute_name(:end_date)
-      tasks = all('.task_list')
-      expect(tasks[0]).to have_content slow_end_date_task.name
-    end
-
-    it 'タスクを優先順位でソートできる' do
-      low_priority_task = create(:task, name: '優先順位が低いタスク', priority: 'low')
-      high_priority_task = create(:task, name: '優先順位が高いタスク', priority: 'high')
-
-      visit tasks_path
-
-      # ソートリンクを押すと初回は昇順でソートされる
-      click_link Task.human_attribute_name(:priority)
-      tasks = all('.task_list')
-      expect(tasks[0]).to have_content low_priority_task.name
-
-      # ソートリンクを押すと2回目は降順でソートされる
-      click_link "#{Task.human_attribute_name(:priority)} ▲"
-      tasks = all('.task_list')
-      expect(tasks[0]).to have_content high_priority_task.name
-    end
-
-    it 'タスク名で検索できる' do
-      non_target_task = create(:task, name: '対象外のタスク')
-      target_task = create(:task, name: '検索対象のタスク')
-
-      visit tasks_path
-
-      # 検索前はどちらも表示
-      tasks = all('.task_list')
-      expect(tasks[0]).to have_content target_task.name
-      expect(tasks[1]).to have_content non_target_task.name
-
-      # タスクを入力して検索
-      fill_in 'q[name_cont]', with: target_task.name
-      within find_by_id('task_search') do
-        click_button :commit
+        visit tasks_path
+        tasks = all('.task_list')
+        expect(tasks[0]).to have_content new_task.name
+        expect(tasks[1]).to have_content old_task.name
       end
 
-      # 検索結果に検索対象のタスクがあり、対象外はない
-      tasks = all('.task_list')
-      expect(tasks[0]).to have_content target_task.name
-      expect(tasks[1]).not_to have_content non_target_task.name
-    end
+      it 'タスクを終了期限でソートできる' do
+        slow_end_date_task = create(:task, name: '期限が遅いタスク', end_date: '2020-02-01')
+        early_end_date_task = create(:task, name: '期限が早いタスク', end_date: '2020-01-01')
 
-    it 'ステータスで検索できる' do
-      non_target_task = create(:task, name: '対象外のタスク')
-      target_task = create(:task, name: '検索対象のタスク', status: 'in_start')
+        visit tasks_path
 
-      visit tasks_path
+        # ソートリンクを押すと初回は昇順でソートされる
+        click_link Task.human_attribute_name(:end_date)
+        tasks = all('.task_list')
+        expect(tasks[0]).to have_content early_end_date_task.name
 
-      # 検索前はどちらも表示
-      tasks = all('.task_list')
-      expect(tasks[0]).to have_content target_task.name
-      expect(tasks[1]).to have_content non_target_task.name
-
-      # ステータス着手中にチェックを入れ検索
-      check 'q_status_in_1'
-      within find_by_id('task_search') do
-        click_button :commit
+        # ソートリンクを押すと2回目は降順でソートされる
+        click_link Task.human_attribute_name(:end_date)
+        tasks = all('.task_list')
+        expect(tasks[0]).to have_content slow_end_date_task.name
       end
 
-      # 検索結果に検索対象のタスクがあり、対象外はない
-      tasks = all('.task_list')
-      expect(tasks[0]).to have_content target_task.name
-      expect(tasks[1]).not_to have_content non_target_task.name
+      it 'タスクを優先順位でソートできる' do
+        low_priority_task = create(:task, name: '優先順位が低いタスク', priority: 'low')
+        high_priority_task = create(:task, name: '優先順位が高いタスク', priority: 'high')
+
+        visit tasks_path
+
+        # ソートリンクを押すと初回は昇順でソートされる
+        click_link Task.human_attribute_name(:priority)
+        tasks = all('.task_list')
+        expect(tasks[0]).to have_content low_priority_task.name
+
+        # ソートリンクを押すと2回目は降順でソートされる
+        click_link "#{Task.human_attribute_name(:priority)} ▲"
+        tasks = all('.task_list')
+        expect(tasks[0]).to have_content high_priority_task.name
+      end
     end
 
-    describe '値の変更' do
+    describe 'search' do
+      it 'タスク名で検索できる' do
+        non_target_task = create(:task, name: '対象外のタスク')
+        target_task = create(:task, name: '検索対象のタスク')
+
+        visit tasks_path
+
+        # 検索前はどちらも表示
+        tasks = all('.task_list')
+        expect(tasks[0]).to have_content target_task.name
+        expect(tasks[1]).to have_content non_target_task.name
+
+        # タスクを入力して検索
+        fill_in 'q[name_cont]', with: target_task.name
+        within find_by_id('task_search') do
+          click_button :commit
+        end
+
+        # 検索結果に検索対象のタスクがあり、対象外はない
+        tasks = all('.task_list')
+        expect(tasks[0]).to have_content target_task.name
+        expect(tasks[1]).not_to have_content non_target_task.name
+      end
+
+      it 'ステータスで検索できる' do
+        non_target_task = create(:task, name: '対象外のタスク')
+        target_task = create(:task, name: '検索対象のタスク', status: 'in_start')
+
+        visit tasks_path
+
+        # 検索前はどちらも表示
+        tasks = all('.task_list')
+        expect(tasks[0]).to have_content target_task.name
+        expect(tasks[1]).to have_content non_target_task.name
+
+        # ステータス着手中にチェックを入れ検索
+        check 'q_status_in_1'
+        within find_by_id('task_search') do
+          click_button :commit
+        end
+
+        # 検索結果に検索対象のタスクがあり、対象外はない
+        tasks = all('.task_list')
+        expect(tasks[0]).to have_content target_task.name
+        expect(tasks[1]).not_to have_content non_target_task.name
+      end
+    end
+
+    describe 'update value' do
       it 'ステータスを変更できる' do
         target_task = create(:task, name: '対象のタスク', status: 'not_started')
         human_in_start = Task.human_attribute_enum_value(:status, 'in_start')
