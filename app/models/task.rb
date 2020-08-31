0cdde6b677fd78c28da9167ee3abf727ab5aa062
necocoa <43537209+necocoa@users.noreply.github.com>
2020-08-31 10:46:03 +0900

    不要な実装を削除

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,14 +4,6 @@ class Task < ApplicationRecord
 
   enum status: { not_started: 0, in_start: 1, completed: 2 }
 
-  scope :end_date_sort, ->(sort_key) do
-    case sort_key&.downcase
-    when 'asc' then order(end_date: :asc)
-    when 'desc' then order('end_date IS NULL, end_date desc')
-    else order(end_date: :asc)
-    end
-  end
-
   def self.statuses_search_values
     statuses.map { |k, v| [human_attribute_enum_value(:status, k), v.to_i] }
   end
