5a87d6a481021b8dad55b062733a78954aefa327
necocoa <43537209+necocoa@users.noreply.github.com>
2020-09-01 18:24:15 +0900

    usersのCRUD作成

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,51 @@
+class Admin::UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def index
+    @users = User.page(params[:page]).order(created_at: :desc)
+  end
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def edit; end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_user_path(@user), notice: 'ユーザーを作成しました。'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: 'ユーザーを編集しました。'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to admin_users_path, notice: 'ユーザーを削除しました。'
+    else
+      redirect_to admin_users_path, alert: 'ユーザーの削除に失敗しました。'
+    end
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  namespace :admin do
+    resources :users, as: :user
+  end
   resources :tasks
 end
