005f94aa65a9d3b8018e9e326e167815a75b614d
necocoa <43537209+necocoa@users.noreply.github.com>
2020-09-01 14:28:46 +0900

    ユーザーとタスクのseedデータを作製

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,24 @@
-user = User.create!(
+def create_tasks(user, times_count)
+  times_count.times do |n|
+    user.tasks.create!(
+      name: "#{user.name}のタスク #{n + 1}",
+      description: "タスク #{n + 1}の説明文。"
+    )
+  end
+end
+
+seed_user = User.create!(
   name: 'seed user',
   email: 'seed@example.com',
-  password_digest: 'password'
+  password: 'seed_password',
+  password_confirmation: 'seed_password'
 )
+create_tasks(seed_user, 5)
 
-5.times do |n|
-  user.tasks.create!(
-    name: "タスク #{n + 1}",
-    description: "タスク #{n + 1}の説明文。"
-  )
-end
+second_seed_user = User.create!(
+  name: 'second seed user',
+  email: 'seed2@example.com',
+  password: 'seed_password',
+  password_confirmation: 'seed_password'
+)
+create_tasks(second_seed_user, 5)
