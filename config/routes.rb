b022544ecaf8faa53bbce4569bd971db589f4b95
necocoa <43537209+necocoa@users.noreply.github.com>
2020-09-01 14:26:00 +0900

    ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'tasks#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :tasks
 end
