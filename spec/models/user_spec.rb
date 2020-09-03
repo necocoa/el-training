053166878ebd8abf3912f2f9aa97728970d3fd57
necocoa <43537209+necocoa@users.noreply.github.com>
2020-09-03 15:42:13 +0900

    管理者が1人の時削除できないテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe User, type: :model do
     let(:user) { build(:user) }
     it { expect(user).to be_valid }
   end
+
+  describe 'callback' do
+    describe '管理者の削除' do
+      let!(:admin_user) { create(:user, admin: true) }
+      context '管理者が1人の時' do
+        it '管理者を削除できない' do
+          expect(User.where(admin: true).size).to eq 1
+          expect { admin_user.destroy! }.to raise_error(ActiveRecord::RecordNotDestroyed)
+        end
+      end
+    end
+  end
 end
