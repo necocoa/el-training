d04ac7aa6be7948fb48454f91beeb832bf6f2aaf
necocoa <43537209+necocoa@users.noreply.github.com>
2020-09-03 13:54:25 +0900

    管理者が0人にならないようにコールバックを追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -36,7 +36,12 @@ class Admin::UsersController < ApplicationController
     if @user.destroy
       redirect_to admin_users_path, notice: 'ユーザーを削除しました。'
     else
-      redirect_to admin_users_path, alert: 'ユーザーの削除に失敗しました。'
+      error_message = if @user.errors.any?
+                        @user.errors.full_messages.join('\n')
+                      else
+                        '不明な理由で、ユーザーの削除に失敗しました。'
+                      end
+      redirect_to admin_users_path, alert: error_message
     end
   end
 
