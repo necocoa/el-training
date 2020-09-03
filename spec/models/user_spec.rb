36b91a36848d945d9542a9bfb3d8a02c6d21a793
necocoa <43537209+necocoa@users.noreply.github.com>
2020-09-03 16:00:40 +0900

    管理者が2人の時正常に削除できるテスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,9 +9,16 @@ RSpec.describe User, type: :model do
   describe 'callback' do
     describe '管理者の削除' do
       let!(:admin_user) { create(:user, admin: true) }
+      context '管理者が2人以上の時' do
+        let!(:second_admin_user) { create(:user, admin: true) }
+        it '管理者を削除できる' do
+          expect(User.where(admin: true).size).to be > 1
+          expect { second_admin_user.destroy! }.to change { User.count }.by(-1)
+        end
+      end
       context '管理者が1人の時' do
         it '管理者を削除できない' do
-          expect(User.where(admin: true).size).to eq 1
+          expect(User.where(admin: true).count).to eq 1
           expect { admin_user.destroy! }.to raise_error(ActiveRecord::RecordNotDestroyed)
         end
       end
