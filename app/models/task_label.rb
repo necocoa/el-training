a882231e8739c02f735d71b00967e92eac65a8ce
necocoa <43537209+necocoa@users.noreply.github.com>
2020-09-04 11:45:50 +0900

    fix typo

--- app/models/task_label.rb
+++ app/models/task_label.rb
@@ -2,5 +2,5 @@ class TaskLabel < ApplicationRecord
   belongs_to :task
   belongs_to :label
 
-  validates :label_id, uniqueness: { scope: %i[lebel_id task_id] }
+  validates :label_id, uniqueness: { scope: %i[label_id task_id] }
 end
